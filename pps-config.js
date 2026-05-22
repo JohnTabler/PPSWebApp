110b11581bb4699b5081b73ae2df7133d790fadc
JohnTabler <131303699+JohnTabler@users.noreply.github.com>
2026-05-22 10:55:25 -0700

    Delete pps-config.js

--- pps-config.js
+++
@@ -1,7 +0,0 @@
-const PPS_CONFIG = {
-  pat:    "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",
-  owner:  "JohnTabler",
-  repo:   "PPSWebApp",
-  branch: "main",
-  path:   "pps-data.js"
-};
