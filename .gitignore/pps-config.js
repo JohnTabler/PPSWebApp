d2dd358571036d9793b88fb19acda300e60400fb
JohnTabler <131303699+JohnTabler@users.noreply.github.com>
2026-05-22 10:34:59 -0700

    Add pps-config.js to .gitignore

---
+++ .gitignore/pps-config.js
@@ -0,0 +1,7 @@
+const PPS_CONFIG = {
+  pat:    "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",
+  owner:  "JohnTabler",
+  repo:   "PPSWebApp",
+  branch: "main",
+  path:   "pps-data.js"
+};
